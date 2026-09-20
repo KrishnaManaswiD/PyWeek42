658a34ffa3ea953fb71660af7c5d19e9f7345e49
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2026-09-20 22:42:20 +1000

    added a game window

--- TheGame/modules/game.py
+++ TheGame/modules/game.py
@@ -1,4 +1,22 @@
 import pyglet
 
+from modules.game_state import GameState
+
 def run():
-    print(pyglet.version)
+    # print(pyglet.version)
+
+    game_state = GameState()
+
+    # create the game window
+    window = pyglet.window.Window(
+        width=game_state.viewport_width,
+        height=game_state.viewport_height,
+        caption="PyWeek42",
+        resizable=False,
+    )
+
+
+
+
+
+    pyglet.app.run()
---
+++ TheGame/modules/game_state.py
@@ -0,0 +1,11 @@
+class GameState(object):
+    """
+    Class to store the current state of the game
+    """
+
+    def __init__(self) -> None:
+
+        # viewport size - this is the size of the window that we see
+        self.viewport_width = 1600
+        self.viewport_height = 800
+
